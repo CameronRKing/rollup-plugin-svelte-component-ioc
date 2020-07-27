6c4e616183ec8162839ea3a376e3793131df4414
Cameron King <cameronking42@gmail.com>
2020-07-27 14:31:06 -0500

    Makes first test slightly more specific.

--- test/index.js
+++ test/index.js
@@ -9,6 +9,8 @@ describe('component-ioc', () => {
     it.only('generates a new file containing a store that links paths to imported component definitions', () => {
         const newSrc = plugin.transform(`<script>import MyCmp from './MyCmp.svelte'</script>`, root + '\\src\\OtherCmp.svelte');
         expect(newSrc.code).to.include("import __DIS__ from '\0component-ioc:component-store';");
+        expect(newSrc.code).to.include("$: MyCmp = $__DIS__['/src/MyCmp']");
+        expect(newSrc.code).not.to.include("import MyCmp from './MyCmp.svelte'");
 
         const storeDefinition = plugin.load('\0component-ioc:component-store');
         expect(storeDefinition).to.include("'/src/MyCmp': srcMyCmp");
