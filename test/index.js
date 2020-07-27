ef66979b397591cc4f06af834c07bd109859390b
Cameron King <cameronking42@gmail.com>
2020-07-27 14:33:33 -0500

    Adds second test

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const root = path.dirname(__dirname);
 const plugin = componentIoc({ root });
 
 describe('component-ioc', () => {
-    it.only('generates a new file containing a store that links paths to imported component definitions', () => {
+    it('generates a new file containing a store that links paths to imported component definitions', () => {
         const newSrc = plugin.transform(`<script>import MyCmp from './MyCmp.svelte'</script>`, root + '\\src\\OtherCmp.svelte');
         expect(newSrc.code).to.include("import __DIS__ from '\0component-ioc:component-store';");
         expect(newSrc.code).to.include("$: MyCmp = $__DIS__['/src/MyCmp']");
@@ -17,11 +17,8 @@ describe('component-ioc', () => {
         expect(storeDefinition).not.to.include("'/src/OtherCmp': srcOtherCmp");
     });
 
-    it('transforms component imports into dependency injections', () => {
-        
-    });
-
     it('transforms component references into dynamic bindings', () => {
-
+        const newSrc = plugin.transform(`<MyCmp attr={not affected}></MyCmp> <OtherCmp attr={not affected }/>`, root + '\\src\\DoesntMatterForThisTest.svelte');
+        expect(newSrc.code).to.equal(`<svelte:component this={MyCmp} attr={not affected}></svelte:component> <svelte:component this={OtherCmp} attr={not affected }/>`);
     });
 });
